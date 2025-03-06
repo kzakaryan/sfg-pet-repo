bcc2b2c652e26501ad296fc68211c98e38c062d9
kzakaryan <kzakaryan@griddynamics.com>
2025-03-06 16:17:04 +0400

    Initial Commit

---
+++ src/main/java/am/demo/sfgpetrepo/SfgPetRepoApplication.java
@@ -0,0 +1,13 @@
+package am.demo.sfgpetrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgPetRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/am/demo/sfgpetrepo/SfgPetRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package am.demo.sfgpetrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
